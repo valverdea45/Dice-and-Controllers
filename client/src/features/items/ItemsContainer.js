deb1c5dec0b0ffd1d5d2df116e0bfb79dd7d6597
Adrian <adrianvalverde45@gmail.com>
2023-11-28 18:37:42 -0500

    created filter for items

--- client/src/features/items/ItemsContainer.js
+++ client/src/features/items/ItemsContainer.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { useSelector } from "react-redux"
 import Search from "../search/Search"
 import ItemsList from "./ItemsList"
@@ -9,17 +9,41 @@ function ItemsContainer() {
     const tableTops = useSelector((state) => state.tabletops.entities)
     const search = useSelector((state) => state.search.value)
     const allItems = videogames.concat(tableTops)
+    const [ filter, setFilter ] = useState("all")
 
+
+    let selectedList = [] 
+
+    if(filter === "all") {
+        selectedList = allItems
+    } else if ( filter === "tabletops") {
+        selectedList = tableTops
+    } else if (filter === "videogames") {
+        selectedList = videogames
+    }
     
 
-    const itemsFiltered = allItems.filter((item) => {
+    const itemsFiltered = selectedList.filter((item) => {
         return item.name.toLowerCase().includes(search[0].toLowerCase())
     }) 
 
 
+    function handleFilterChange(e) {
+        setFilter(e.target.value)
+    }
+
     return (
         <div>
             <Search/>
+            <br/>
+
+            <label>Filter By Game Type:</label>
+            <select onChange={handleFilterChange} value={filter}>
+                <option value="all">show all</option>
+                <option value="tabletops">show table tops</option>
+                <option value="videogames">show video games</option>
+            </select>
+
             <ItemsList itemsFiltered={itemsFiltered}/>
         </div>
     )
